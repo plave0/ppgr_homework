9e0102da3995be4ab7926b0de4541b5edfc51e47
Pavle Ciric <pavle@ciric.me>
2023-02-24 10:36:50 +0100

    Fixes

--- lib/pg2d.py
+++ lib/pg2d.py
@@ -1,4 +1,5 @@
 
+import numpy as np
 from collections import namedtuple
 
 AffinePoint2D = namedtuple('AffinePoint2D', 'x y')
@@ -29,3 +30,73 @@ def join_projective_points(point1: ProjectivePoint2D, point2: ProjectivePoint2D)
 def meet_projective_lines(line1: ProjectiveLine2D, line2: ProjectiveLine2D) -> ProjectivePoint2D:
     point = cross_product_3d(line1, line2)
     return ProjectivePoint2D(*(point))
+
+def nevidljiva_tacka(
+    p1:ProjectivePoint2D,
+    p2:ProjectivePoint2D,
+    p3:ProjectivePoint2D,
+    p5:ProjectivePoint2D,
+    p6:ProjectivePoint2D,
+    p7:ProjectivePoint2D,
+    p8:ProjectivePoint2D,
+):
+
+    xb_1 = meet_projective_lines(
+        join_projective_points(p2, p6),
+        join_projective_points(p1, p5)
+    )
+    xb_2 = meet_projective_lines(
+        join_projective_points(p2, p6),
+        join_projective_points(p3, p7)
+    )
+    xb_3 = meet_projective_lines(
+        join_projective_points(p3, p7),
+        join_projective_points(p1, p5)
+    )
+
+    xb = ProjectivePoint2D(*(
+        map(lambda i, j, k: i + j + k, xb_1, xb_2, xb_3)
+    ))
+    xb = ProjectivePoint2D(*(
+        map(lambda x: x/3, xb)
+    ))
+
+    yb_1 = meet_projective_lines(
+        join_projective_points(p7, p8), 
+        join_projective_points(p6, p5)
+    )
+    yb_2 = meet_projective_lines(
+        join_projective_points(p7, p8),
+        join_projective_points(p1, p2)
+    )
+    yb_3 = meet_projective_lines(
+        join_projective_points(p5, p6),
+        join_projective_points(p1, p2)
+    )
+
+    yb = ProjectivePoint2D(*(
+        map(lambda i, j, k: i + j + k, yb_1, yb_2, yb_3)
+    ))
+    yb = ProjectivePoint2D(*(
+        map(lambda x : x/3, yb)
+    ))
+
+    p4 = meet_projective_lines(
+        join_projective_points(yb, p3),
+        join_projective_points(xb, p8)
+    )
+
+    return p4
+
+def nevidljiva_tacka_2(
+    p1:ProjectivePoint2D,
+    p2:ProjectivePoint2D,
+    p3:ProjectivePoint2D,
+    p5:ProjectivePoint2D,
+    p6:ProjectivePoint2D,
+    p7:ProjectivePoint2D,
+    p8:ProjectivePoint2D,
+):
+
+    p4 = nevidljiva_tacka(p1, p2, p3, p5, p6, p7, p8)
+    return np.array(p4)
